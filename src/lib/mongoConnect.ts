2d321be283ad6dfa0f44e4846907eac0bc7b6e7d
JimmyDeveloperMan <jimmy.moftah@gmail.com>
2024-01-26 20:41:35 +0200

    Use `MongoClient` from `mongodb` library.

--- src/lib/mongoConnect.ts
+++ src/lib/mongoConnect.ts
@@ -1,28 +1,27 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb";
 
 if (!process.env.MONGO_URL) {
-  throw new Error('Invalid/Missing environment variable: "MONGO_URL"')
+  throw new Error('Invalid/Missing environment variable: "MONGO_URL"');
 }
+declare const global: {
+  _mongoClientPromise: Promise<MongoClient>;
+};
+const url = process.env.MONGO_URL;
+const options = {};
 
-const url = process.env.MONGO_URL
-const options = {}
-
-let client
-let clientPromise: Promise<MongoClient>
+let client;
+let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
-  
-  if (!global._mongoClientPromise ) {
-    client = new MongoClient(url, options)
-    global._mongoClientPromise = client.connect()
-    
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(url, options);
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(url, options)
-  clientPromise = client.connect()
+  client = new MongoClient(url, options);
+  clientPromise = client.connect();
 }
 
-
-export default clientPromise
+export default clientPromise;
